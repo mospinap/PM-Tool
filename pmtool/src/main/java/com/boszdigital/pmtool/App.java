22a0cf3d43ccb4438c34ff02955ad2e95b042b0d
Daniel Hoyos <daniel.hoyos@boszdigital.com>
2013-07-25 08:15:33 -0500

    Project Setup

---
+++ pmtool/src/main/java/com/boszdigital/pmtool/App.java
@@ -0,0 +1,14 @@
+package com.boszdigital.pmtool;
+
+/**
+ * PM Tool [Project Manager Tool]
+ * 
+ * This tool is designed to support the processes of the Project Managers at
+ * Bosz Digital.
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		
+	}
+}
